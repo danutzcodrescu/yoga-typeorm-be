9ff9c530f2927be50b674f3900dcb571e780e085
danutzcodrescu <danutzcodrescu@gmail.com>
2018-06-24 11:31:52 +0200

    added partial support for apollo engine

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
-import "reflect-metadata";
-import { createConnection } from "typeorm";
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
 // import { User } from "./entity/User";
 
-import { GraphQLServer } from "graphql-yoga";
-// import { ApolloEngine } from "apollo-engine";
+import { GraphQLServer } from 'graphql-yoga';
+import { ApolloEngine } from 'apollo-engine';
 // tslint:disable-next-line
 import * as cookieParser from "cookie-parser";
-import { isNil } from "lodash";
+import { isNil } from 'lodash';
 
 // tslint:disable-next-line
 require("dotenv").config();
 
-import resolvers from "./resolvers/index.resolvers";
-import typeDefs from "./schema/schema";
+import resolvers from './resolvers/index.resolvers';
+import typeDefs from './schema/schema';
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
   context: ({ request, response }) => {
     let token;
+    console.log(request.cookies);
     if (!isNil(request.cookies.access_token)) {
       token = request.cookies.access_token;
     }
@@ -29,25 +30,32 @@ const server = new GraphQLServer({
   }
 });
 
-// const engine = new ApolloEngine({
-//     apiKey: 'service:danutzcodrescu-9658:WkAUrgrGg4zwqjo6KM3Q2w'
-// });
-
-// server.express.use(
-//   session({
-//     name: "access_token",
-//     secret: process.env.SECRET_COOKIE,
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//       secure: process.env.NODE_ENV === "production",
-//       maxAge: ms("1d"),
-//       httpOnly: true
-//     }
-//   })
-// );
 server.express.use(cookieParser());
-server.start(() => console.log("Server is running on localhost:4000"));
+
+if (process.env.ENGINE) {
+  const engine = new ApolloEngine({
+    apiKey: process.env.APOLLO_ENGINE_KEY
+  });
+
+  const httpServer = server.createHttpServer({
+    tracing: true,
+    cacheControl: true
+  });
+
+  engine.listen(
+    {
+      port: 4000,
+      httpServer,
+      graphqlPaths: ['/']
+    },
+    () =>
+      console.log(
+        `Server with Apollo Engine is running on http://localhost:4000`
+      )
+  );
+} else {
+  server.start(() => console.log('Server is running on localhost:4000'));
+}
 
 createConnection()
   .then(() => {
@@ -65,6 +73,6 @@ createConnection()
     // console.log("Loaded users: ", users);
 
     // console.log("Here you can setup and run express/koa/any other framework.");
-    console.log("connection to db established");
+    console.log('connection to db established');
   })
   .catch(error => console.log(error));
--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -1,14 +1,14 @@
-import { IResolvers } from "graphql-tools";
-import { User as UserModel } from "../entity/User";
-import { User } from "../../types/schemas/types";
-import { getRepository } from "typeorm";
-import { isNil } from "lodash";
-import { ApolloError } from "apollo-errors";
-import * as argon from "argon2";
-import { Response } from "express";
-import * as jwt from "jsonwebtoken";
-import * as fs from "fs";
-import * as path from "path";
+import { IResolvers } from 'graphql-tools';
+import { User as UserModel } from '../entity/User';
+import { User } from '../../types/schemas/types';
+import { getRepository } from 'typeorm';
+import { isNil } from 'lodash';
+import { ApolloError } from 'apollo-errors';
+import * as argon from 'argon2';
+import { Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+import * as fs from 'fs';
+import * as path from 'path';
 // tslint:disable-next-line
 const ms = require("ms");
 
@@ -24,14 +24,14 @@ const userResolver: IResolvers = {
       console.log(decoded);
       if (isNil(decoded)) {
         return new ApolloError(
-          "must_authenticate",
-          { message: "Not authenticated" },
-          { message: "Not authenticated" }
+          'must_authenticate',
+          { message: 'Not authenticated' },
+          { message: 'Not authenticated' }
         );
       }
       return getRepository(UserModel)
-        .createQueryBuilder("user")
-        .where("user.email = :email OR user.username = :username", {
+        .createQueryBuilder('user')
+        .where('user.email = :email OR user.username = :username', {
           email,
           username
         })
@@ -60,26 +60,26 @@ const userResolver: IResolvers = {
       });
       if (isNil(user)) {
         return new ApolloError(
-          "not_found",
-          { message: "User not found" },
-          { message: "User not found" }
+          'not_found',
+          { message: 'User not found' },
+          { message: 'User not found' }
         );
       }
       if (!(await argon.verify(user.password, password))) {
         return new ApolloError(
-          "not_found",
-          { message: "Password is wrong" },
-          { message: "Password is wrong" }
+          'not_found',
+          { message: 'Password is wrong' },
+          { message: 'Password is wrong' }
         );
       }
       const cert = fs.readFileSync(
-        path.join(__dirname, "../../certs", "private.key")
+        path.join(__dirname, '../../certs', 'private.key')
       );
-      const token = jwt.sign({ exp: ms("1d") / 1000, user }, cert, {
-        algorithm: "RS256"
+      const token = jwt.sign({ exp: ms('1d') / 1000, user }, cert, {
+        algorithm: 'RS256'
       });
-      response.cookie("access_token", token, {
-        maxAge: ms("1d") / 1000,
+      response.cookie('access_token', token, {
+        maxAge: ms('1d') / 1000,
         httpOnly: true
       });
       return user;
