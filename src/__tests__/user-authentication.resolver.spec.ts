87875984642dbd16b5b5c1bb37f3f523aa304522
danutzcodrescu <danutzcodrescu@gmail.com>
2018-08-24 22:30:39 +0200

    some refactoring

--- src/__tests__/user-authentication.resolver.spec.ts
+++ src/__tests__/user-authentication.resolver.spec.ts
@@ -1,8 +1,5 @@
 import * as faker from 'faker';
 import axios from 'axios';
-import { createConnection } from 'typeorm';
-import { userRepo } from '../helpers/userRepo';
-import { User } from '../entity/User';
 
 const instance = axios.create({
   baseURL: 'http://localhost:4000',
@@ -130,109 +127,5 @@ describe('user logic flow', () => {
       expect(err).toBeDefined();
     }
     done();
-  });
-});
-
-describe('add friends', () => {
-  const variables = {
-    user1: {
-      email: faker.internet.email(),
-      username: faker.internet.userName()
-    },
-    user2: {
-      email: faker.internet.email(),
-      username: faker.internet.userName()
-    }
-  };
-  let User1: User;
-  let User2: User;
-  beforeAll(async () => {
-    await createConnection('test');
-
-    const user1 = new User();
-    user1.email = variables.user1.email;
-    user1.username = variables.user1.username;
-    user1.password = 'test';
-
-    const user2 = new User();
-    user2.email = variables.user2.email;
-    user2.username = variables.user2.username;
-    user2.password = 'test';
-
-    [User1, User2] = await Promise.all([
-      userRepo().save(user1),
-      userRepo().save(user2)
-    ]);
-
-    const loginMutation = `
-      mutation($email: String!) {
-        login(email: $email, password: "test") {
-          email,
-          username,
-          status
-        }
-      }`;
-    const resp = await instance.post('/', {
-      variables: {
-        email: User1.email,
-        password: 'test'
-      },
-      query: loginMutation
-    });
-    cookie = resp.headers['set-cookie'][0];
-  });
-
-  it('should add a friend', async () => {
-    const addFriendMutation = `
-      mutation($id: String!) {
-        addFriend(id: $id) {
-           friends {
-             email
-           }
-        }
-      }`;
-
-    const friends = await instance.post(
-      '/',
-      {
-        variables: {
-          id: User2.id
-        },
-        query: addFriendMutation
-      },
-      { headers: { Cookie: cookie } }
-    );
-    expect(friends.data.data.addFriend.friends).toContainEqual({
-      email: User2.email
-    });
-    const user = await userRepo().findOne({ id: User2.id });
-    expect(user.friends).toContain(User1.id);
-  });
-
-  it('should not existing friend', async () => {
-    const addFriendMutation = `
-      mutation($id: String!) {
-        addFriend(id: $id) {
-           friends {
-             email
-           }
-        }
-      }`;
-
-    const friends = await instance.post(
-      '/',
-      {
-        variables: {
-          id: User2.id
-        },
-        query: addFriendMutation
-      },
-      { headers: { Cookie: cookie } }
-    );
-    expect(friends.data.data.addFriend.friends).toContainEqual({
-      email: User2.email
-    });
-    const user = await userRepo().findOne({ id: User2.id });
-    expect(user.friends).toContain(User1.id);
   });
 });
--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -1,148 +1,17 @@
 import { IResolvers } from 'graphql-tools';
-import { User as UserModel, Status } from '../entity/User';
-import { omit } from 'lodash';
-import * as jwt from 'jsonwebtoken';
-import * as fs from 'fs';
-import * as path from 'path';
-import { userRepo } from '../helpers/userRepo';
-import {
-  RegisterMutationArgs,
-  LogoutMutationArgs,
-  AddFriendMutationArgs,
-  UserQueryArgs,
-  StatusChange,
-  StatusSubscriptionArgs
-} from 'types/schemas';
+import { User as UserModel } from '../entity/User';
+import { StatusChange, StatusSubscriptionArgs } from 'types/schemas';
 
-import { addDays, getTime } from 'date-fns';
 import { AppContext } from 'types/utilities';
 import { withFilter } from 'graphql-yoga';
+import { STATUS } from '../globals/constants';
+import { UserQuery } from './user.query';
+import { UserMutation } from './user.mutation';
 
-const STATUS = 'STATUS';
-
-const cert = fs.readFileSync(
-  path.join(
-    __dirname,
-    '../../certs',
-    process.env.NODE_ENV === 'test' ? 'private.test.key' : 'private.prod.key'
-  )
-);
-
-const userResolver: IResolvers = {
+const resolver: IResolvers = {
   User: {
     friends: (user: UserModel, _, { userLoader }: AppContext) =>
       user.friends.length > 0 ? userLoader.loadMany(user.friends) : []
-  },
-  Query: {
-    users: () => userRepo().find(),
-    user: (_, { email, username }: UserQueryArgs) => {
-      return userRepo()
-        .createQueryBuilder('user')
-        .where('user.email = :email OR user.username = :username', {
-          email,
-          username
-        })
-        .getOne();
-    }
-  },
-
-  Mutation: {
-    register: async (
-      _,
-      { email, username, password }: RegisterMutationArgs
-    ) => {
-      const user = new UserModel();
-      user.email = email;
-      user.password = password;
-      user.username = username;
-      let resp;
-      try {
-        resp = await userRepo().save(user);
-      } catch (e) {
-        if (e.message.includes('duplicate key')) {
-          return new Error('Duplicate email');
-        } else {
-          return new Error('Cannot create');
-        }
-      }
-      return resp;
-    },
-
-    login: async (_, __, { response, user, pubsub }: AppContext) => {
-      user.status = Status.active;
-      let safeUser;
-      try {
-        safeUser = await userRepo().save(user);
-        safeUser = omit(user, ['password']);
-      } catch (e) {
-        console.log(e);
-      }
-
-      const expires = addDays(new Date(), 1);
-      const seconds = getTime(expires) / 1000;
-      const token = jwt.sign({ exp: seconds, user: safeUser }, cert, {
-        algorithm: 'RS256'
-      });
-
-      response.cookie('access_token', token, {
-        httpOnly: true,
-        expires,
-        maxAge: 1000 * 60 * 60 * 24
-      });
-      pubsub.publish(STATUS, <{ status: StatusChange }>{
-        status: {
-          userId: user.id,
-          status: Status.active
-        }
-      });
-      return safeUser;
-    },
-
-    logout: async (
-      _,
-      { email }: LogoutMutationArgs,
-      { response, user, pubsub }: AppContext
-    ) => {
-      if (email !== user.email) {
-        return new Error('no simmilar emails');
-      }
-      try {
-        await userRepo()
-          .createQueryBuilder()
-          .update()
-          .set({ status: Status.inactive })
-          .where('email = :email', { email })
-          .execute();
-      } catch (e) {
-        return new Error('log off failed');
-      }
-      response.clearCookie('access_token');
-      pubsub.publish(STATUS, <{ status: StatusChange }>{
-        status: {
-          userId: user.id,
-          status: Status.inactive
-        }
-      });
-      return 'logged out';
-    },
-
-    addFriend: async (
-      _,
-      { id }: AddFriendMutationArgs,
-      { user }: AppContext
-    ) => {
-      if (!user.friends.includes(id)) {
-        try {
-          await Promise.all([
-            userRepo().addFriend(id, user.id),
-            userRepo().addFriend(user.id, id)
-          ]);
-        } catch (e) {
-          console.log(e);
-        }
-      }
-      return userRepo().findOne({ id: user.id });
-    }
   },
 
   Subscription: {
@@ -166,4 +35,5 @@ export interface JWT {
   user: any;
   iat: number;
 }
-export default userResolver;
+
+export default Object.assign(resolver, UserQuery, UserMutation);
